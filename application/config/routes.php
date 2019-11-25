774af00c1f3514d2518de798748e3a4b61d0f0cb
anandavj <anandaprabu.trityavijaya@gmail.com>
2019-11-26 06:28:48 +0700

    add login page

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,7 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['login'] = 'Login';
 
 
 //Pages
+$route['register'] = 'pages/register';
+$route['login'] = 'pages/login';
 
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg d-flex justify-content-between navbar-my navbar-dark bg-dark mb-5">
-	<a class="navbar-brand" href="#">
+	<a class="navbar-brand" href="<?=base_url()?>">
 		<div class="d-flex align-items-center">
 			<img src="" class="d-inline-block align-top" alt="">
 			<span class="ml-3 text-light">CMS</span>
 		</div>
 	</a>
 	<div class="float-right">
-		<a href="#">
+		<a href="<?=base_url('login')?>">
 			<button type="button" class="btn btn-success">Login</button>
 		</a>
 	</div>
